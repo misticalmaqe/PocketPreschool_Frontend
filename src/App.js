39008c9685e3f25e8b64805cc0ca9b450aff115f
root <khaira_jaipal@hotmail.com>
2023-12-01 12:27:43 +0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+//import UserProvider from "./Provider/UserProvider";
 
-function App() {
+// Import your componets for admin account...
+import LogInPage from "./Pages/LogInPage";
+import ForgotPassword from "./Pages/ForgotPassword";
+import HomePage from "./Pages/HomePage";
+import Chat from "./Pages/Chat";
+import Activity from "./Pages/Activity";
+import ProfilePage from "./Pages/ProfilePage";
+import SignUpPage from "./Pages/SignUpPage";
+import ErrorPage from "./Pages/ErrorPage";
+
+//Import you componets from parent account...
+// import { LogInPage } from "./Pages/LogInPage";
+// import { ForgotPassword } from "./Pages/ForgotPassword";
+// import { HomePage } from "./Pages/HomePage";
+// import { Chat } from "./Pages/Chat";
+// import { Activity } from "./Pages/Activity";
+// import { ProfilePage } from "./Pages/ProfilePage";
+// import { ErrorPage } from "./Pages/ErrorPage";
+
+//--------------STYLING--------------//
+import "./App.css";
+
+// App componets
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <UserProvider>
+    <Routes>
+      <Route path="/" element={<LogInPage />} />
+      <Route path="/forgetpassword" element={<ForgotPassword />} />
+      <Route path="/HomePage" element={<HomePage />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/activity" element={<Activity />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+    // </UserProvider>
   );
 }
-
-export default App;
---
+++ src/Pages/ForgotPassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ForgotPassword = () => {
+  return <h1>Forgot Password page</h1>;
+};
+
+export default ForgotPassword;
