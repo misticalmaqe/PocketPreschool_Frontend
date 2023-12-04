c3eefd4e6ca1a8efcfd3793ee4e6dd0edb8dde3f
root <khaira_jaipal@hotmail.com>
2023-12-04 16:17:11 +0800

    feature/headers

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,23 @@
 // HomePage.js
-import React from "react";
-import NavBar from "../Components/NavBar"; // Adjust the import path
+import React, { useState } from "react";
+import NavBar from "../Components/NavBar";
+import AppHeader from "../Components/AppHeader";
 
 const HomePage = () => {
+  const [isAdmin] = useState(true);
+
   return (
     <div>
-      <NavBar />
-      <h1>Home page</h1>
+      <AppHeader isAdmin={isAdmin}>
+        <div
+          className={`AppHeader ${
+            isAdmin ? "bg-adminBackground" : "bg-parentBackground"
+          } fixed top-0 left-0 w-full flex justify-center p-10`}
+        >
+          <NavBar />
+          <h1 className="text-5xl font-bold">Home</h1>
+        </div>
+      </AppHeader>
     </div>
   );
 };
--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,12 +1,25 @@
-import React from "react";
+// ProfilePage.js
+import React, { useState } from "react";
 import NavBar from "../Components/NavBar";
+import AppHeader from "../Components/AppHeader";
 
 const ProfilePage = () => {
+  const [isAdmin] = useState(true);
+
   return (
     <div>
-      <NavBar />
-      <h1>Profile Page</h1>;
+      <AppHeader isAdmin={isAdmin}>
+        <div
+          className={`AppHeader ${
+            isAdmin ? "bg-adminBackground" : "bg-parentBackground"
+          } fixed top-0 left-0 w-full flex justify-center p-10`}
+        >
+          <NavBar />
+          <h1 className="text-5xl font-bold">Profile Page</h1>
+        </div>
+      </AppHeader>
     </div>
   );
 };
+
 export default ProfilePage;
