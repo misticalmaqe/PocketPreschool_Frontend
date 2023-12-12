042e5cdc093e39572f298a481b261688772bc873
root <khaira_jaipal@hotmail.com>
2023-12-12 11:55:02 +0800

    fixed the chat to allow connection before entering the chatroom

--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -1,8 +1,106 @@
+// import React, { useState, useEffect, useContext } from "react";
+// import apiRequest from "../Api/index";
+// import NavBar from "../Components/NavBar";
+// import AppHeader from "../Components/AppHeader";
+// import Message from "../Pages/Message";
+// import { UserContext } from "../Provider/UserProvider";
+// import { useNavigate } from "react-router-dom";
+
+// function formatDate(dateTimeString) {
+//   const options = {
+//     day: "numeric",
+//     month: "numeric",
+
+//     hour: "numeric",
+//     minute: "numeric",
+//   };
+//   return new Date(dateTimeString).toLocaleDateString("en-SG", options);
+// }
+
+// const Chat = () => {
+//   const { isAdmin, user, child } = useContext(UserContext);
+//   const [chatrooms, setChatrooms] = useState([]);
+//   const navigate = useNavigate();
+//   const BEURL = process.env.REACT_APP_BE_URL;
+
+//   //function to fetch chatrooms from BE for TEACHERS
+//   const fetchTeachersChat = async () => {
+//     if (user && user.id) {
+//       const response = await apiRequest.get(`${BEURL}/chat/teacher/${user.id}`);
+//       setChatrooms(response.data);
+//     }
+//   };
+
+//   const fetchChildrensChat = async () => {
+//     if (user && user.id) {
+//       //get childrensIds from children table
+//       const childrenRes = await apiRequest.get(
+//         `${BEURL}/user/child/${user.id}`
+//       );
+//       const childrenIds = childrenRes.data.map((item) => item.id);
+//       //fetch chatrooms with childrens Ids
+//       const response = await apiRequest.get(
+//         `${BEURL}/chat/child/${childrenIds}`
+//       );
+//       setChatrooms(response.data);
+//     }
+//   };
+
+//   useEffect(() => {
+//     if (isAdmin === true) {
+//       fetchTeachersChat();
+//     } else {
+//       fetchChildrensChat();
+//     }
+//   }, [child, user, isAdmin]);
+
+//   return (
+//     <div>
+//       <AppHeader input="Chat" />
+//       <NavBar />
+//       <div className="flex justify-center items-center h-screen">
+//         <div className="w-screen p-4  text-center mt-8">
+//           <h2 className="text-lg font-semibold mb-4">Chatrooms</h2>
+//           <ul className="list-none">
+//             {child.map((child) => (
+//               <React.Fragment key={child.id}>
+//                 <li
+//                   onClick={() => navigate(`/chat/${child.id}/Message`)}
+//                   className="relative group mb-2 flex justify-between items-center"
+//                   style={{ position: "relative", marginBottom: "0.5rem" }}
+//                 >
+//                   <div className="flex items-center">
+//                     <div
+//                       className="w-4 h-4  rounded-full mr-2" // replace code here to remove the circle
+//                       // Adjust the size and color of the circle as needed
+//                     >
+//                       <img src={child.displayPhoto} alt="display photo" />
+//                     </div>
+//                     <div>{child.fullName}</div>
+//                   </div>
+//                   <div className="text-xs text-gray-500">
+//                     {formatDate(
+//                       chatrooms.find((room) => room.childrenId === child.id)
+//                         ?.createdAt
+//                     )}
+//                     {/*  take create at and last message from chat table */}
+//                   </div>
+//                 </li>
+//                 <hr style={{ width: "100%" }} />
+//               </React.Fragment>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Chat;
 import React, { useState, useEffect, useContext } from "react";
 import apiRequest from "../Api/index";
 import NavBar from "../Components/NavBar";
 import AppHeader from "../Components/AppHeader";
-import Message from "../Pages/Message";
 import { UserContext } from "../Provider/UserProvider";
 import { useNavigate } from "react-router-dom";
 
@@ -10,7 +108,6 @@ function formatDate(dateTimeString) {
   const options = {
     day: "numeric",
     month: "numeric",
-
     hour: "numeric",
     minute: "numeric",
   };
@@ -23,7 +120,6 @@ const Chat = () => {
   const navigate = useNavigate();
   const BEURL = process.env.REACT_APP_BE_URL;
 
-  //function to fetch chatrooms from BE for TEACHERS
   const fetchTeachersChat = async () => {
     if (user && user.id) {
       const response = await apiRequest.get(`${BEURL}/chat/teacher/${user.id}`);
@@ -33,12 +129,10 @@ const Chat = () => {
 
   const fetchChildrensChat = async () => {
     if (user && user.id) {
-      //get childrensIds from children table
       const childrenRes = await apiRequest.get(
         `${BEURL}/user/child/${user.id}`
       );
       const childrenIds = childrenRes.data.map((item) => item.id);
-      //fetch chatrooms with childrens Ids
       const response = await apiRequest.get(
         `${BEURL}/chat/child/${childrenIds}`
       );
@@ -59,7 +153,7 @@ const Chat = () => {
       <AppHeader input="Chat" />
       <NavBar />
       <div className="flex justify-center items-center h-screen">
-        <div className="w-screen p-4  text-center mt-8">
+        <div className="w-screen p-4 text-center mt-8">
           <h2 className="text-lg font-semibold mb-4">Chatrooms</h2>
           <ul className="list-none">
             {child.map((child) => (
@@ -70,10 +164,9 @@ const Chat = () => {
                   style={{ position: "relative", marginBottom: "0.5rem" }}
                 >
                   <div className="flex items-center">
-                    <div
-                      className="w-4 h-4 bg-blue-500 rounded-full mr-2" // replace code here to remove the circle
-                      // Adjust the size and color of the circle as needed
-                    ></div>
+                    <div className="w-4 h-4  rounded-full mr-2">
+                      <img src={child.displayPhoto} alt="display photo" />
+                    </div>
                     <div>{child.fullName}</div>
                   </div>
                   <div className="text-xs text-gray-500">
@@ -81,7 +174,6 @@ const Chat = () => {
                       chatrooms.find((room) => room.childrenId === child.id)
                         ?.createdAt
                     )}
-                    {/*  take create at and last message from chat table */}
                   </div>
                 </li>
                 <hr style={{ width: "100%" }} />
--- src/Pages/Message.js
+++ src/Pages/Message.js
@@ -1,11 +1,147 @@
-// Message.js
+// import React, {
+//   useState,
+//   useEffect,
+//   useCallback,
+//   useContext,
+//   useMemo,
+// } from "react";
+// import { io } from "socket.io-client";
+// import { useParams } from "react-router-dom";
+// import { UserContext } from "../Provider/UserProvider";
+// import apiRequest from "../Api/index";
 
+// const Message = () => {
+//   const { isAdmin, user } = useContext(UserContext);
+//   const [messages, setMessages] = useState([]);
+//   const [inputMessage, setInputMessage] = useState("");
+//   let { chatroomId } = useParams();
+//   const BEURL = process.env.REACT_APP_BE_URL;
+//   const socket = useMemo(() => io(BEURL, { reconnection: true }), []);
+
+//   // Existing code for handling message sending
+//   const handleSendMessage = useCallback(
+//     async (e) => {
+//       if (e.key === "Enter" && inputMessage.trim() !== "") {
+//         e.preventDefault();
+
+//         const newMessage = { text: inputMessage, sender: "user" };
+
+//         setMessages((prevMessages) => [...prevMessages, newMessage]);
+//         setInputMessage("");
+
+//         socket.emit("send-message", newMessage, chatroomId);
+
+//         console.log("Message sent to server:", newMessage);
+//       }
+//     },
+//     [inputMessage, setMessages, setInputMessage, socket, chatroomId]
+//   );
+
+//   // Existing code for handling received messages
+//   useEffect(() => {
+//     const handleReceiveMessage = (message) => {
+//       console.log("Received message from server:", message);
+//       setMessages((prevMessages) => [...prevMessages, message]);
+//     };
+
+//     socket.on("receive-message", handleReceiveMessage);
+
+//     return () => {
+//       socket.off("receive-message", handleReceiveMessage);
+//     };
+//   }, []);
+
+//   // Existing code for handling socket connection
+//   useEffect(() => {
+//     socket.on("connect", () => {
+//       console.log(`Connected to server with id: ${socket.id}`);
+//     });
+
+//     return () => {
+//       socket.off("connect");
+//     };
+//   }, []);
+
+//   // Existing code for joining a room
+//   const handleJoinRoom = () => {
+//     console.log(chatroomId);
+//     socket.emit("join-room", chatroomId);
+//   };
+
+//   // Existing code for handling key press
+//   const handleKeyPress = (e) => {
+//     if (
+//       e.key === "Enter" &&
+//       !e.shiftKey &&
+//       !e.ctrlKey &&
+//       !e.metaKey &&
+//       !e.altKey
+//     ) {
+//       e.preventDefault();
+//       handleSendMessage(e);
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col h-screen">
+//       <div className="p-4 mb-4 max-w-screen-xl mx-auto overflow-y-auto flex-grow">
+//         {messages.map((msg, index) => (
+//           <div
+//             key={index}
+//             className={`message-container w-full ${
+//               msg.sender === "user" ? "justify-end" : "justify-start"
+//             }`}
+//           >
+//             <div
+//               className={`message-bubble p-2 rounded mb-2 ${
+//                 msg.sender === "user"
+//                   ? "bg-userBackground"
+//                   : "bg-adminBackground"
+//               }`}
+//             >
+//               {msg.text}
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="flex-grow" />
+
+//       <div className="flex items-center w-full p-4">
+//         <input
+//           type="text"
+//           placeholder="Type your message..."
+//           className="border p-2 mr-2 flex-grow"
+//           value={inputMessage}
+//           onChange={(e) => setInputMessage(e.target.value)}
+//           onKeyDown={handleKeyPress}
+//         />
+
+//         <button
+//           onClick={handleSendMessage}
+//           className="p-2 rounded bg-blue-500 text-white"
+//         >
+//           Send
+//         </button>
+
+//         <button
+//           onClick={handleJoinRoom}
+//           className="p-2 rounded bg-green-500 text-white ml-2"
+//         >
+//           Join Room
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Message;
 import React, {
-  useState,
   useEffect,
+  useState,
+  useMemo,
   useCallback,
   useContext,
-  useMemo,
 } from "react";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
@@ -16,9 +152,9 @@ const Message = () => {
   const { isAdmin, user } = useContext(UserContext);
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState("");
-  let { chatroomId } = useParams();
+  const { chatroomId } = useParams();
   const BEURL = process.env.REACT_APP_BE_URL;
-  const socket = useMemo(() => io(BEURL), []);
+  const socket = useMemo(() => io(BEURL, { reconnection: true }), []);
 
   const handleSendMessage = useCallback(
     async (e) => {
@@ -54,17 +190,14 @@ const Message = () => {
   useEffect(() => {
     socket.on("connect", () => {
       console.log(`Connected to server with id: ${socket.id}`);
+      // Join the room when component mounts
+      socket.emit("join-room", chatroomId);
     });
 
     return () => {
       socket.off("connect");
     };
-  }, [socket]);
-
-  const handleJoinRoom = () => {
-    console.log(chatroomId);
-    socket.emit("join-room", chatroomId);
-  };
+  }, [socket, chatroomId]);
 
   const handleKeyPress = (e) => {
     if (
@@ -77,6 +210,11 @@ const Message = () => {
       e.preventDefault();
       handleSendMessage(e);
     }
+  };
+
+  const handleJoinRoom = () => {
+    console.log(chatroomId);
+    socket.emit("join-room", chatroomId);
   };
 
   return (
@@ -119,13 +257,6 @@ const Message = () => {
           className="p-2 rounded bg-blue-500 text-white"
         >
           Send
-        </button>
-
-        <button
-          onClick={handleJoinRoom}
-          className="p-2 rounded bg-green-500 text-white ml-2"
-        >
-          Join Room
         </button>
       </div>
     </div>
